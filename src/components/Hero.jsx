bbf8ec42a8f38e9a542aeeea5c5c196313622d23
Ann Thomas <ann-thomas2010@hotmail.com>
2025-12-26 17:37:19 -0500

    Hero styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,29 @@
 const Hero = () => {
   return (
-    <section>
-      <h1>Ann Thomas</h1>
-      <p>Beginner Web Developer</p>
-      <p>React + Django</p>
-      <p>Learning Everyday</p>
+    <section className="min-h-screen flex flex-col justify-center items-center text-center px-6 ">
+      <h1 className="text-5ml md:text-6xl font-bold tracking-tight">
+        Ann Thomas
+      </h1>
+      <p className="mt-4 text-xl text-gray-400">Beginner Web Developer</p>
+
+      <p className="mt-2 text-gray-500 max-w-xl">
+        I build full-stack applications using React and Django and I’m
+        constantly learning and improving.
+      </p>
+      <div className="mt-8 flex gap-4">
+        <a
+          href="#projects"
+          className="px-6 py-3 bg-indigo-600 rounded-lg hover:bg-indigo-500 transition"
+        >
+          View projects
+        </a>
+        <a
+          href="#contact"
+          className="px-6 py-3 border-gray-600 rounded-lg hover:border-gray-400 transition"
+        >
+          Contact
+        </a>
+      </div>
     </section>
   );
 };
