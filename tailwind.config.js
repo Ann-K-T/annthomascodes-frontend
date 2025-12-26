29b63453e45a7a81cc4d12645a77eababb56edd0
Ann Thomas <ann-thomas2010@hotmail.com>
2025-12-26 18:10:49 -0500

    tailwind change

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html', // all HTML files in root
+    './src/**/*.{js,ts,jsx,tsx}', // all JS/TS/JSX/TSX files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
