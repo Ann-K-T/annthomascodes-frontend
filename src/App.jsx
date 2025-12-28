af4e8d942980e06d5a678dcb9d296b6d54605372
Ann Thomas <ann-thomas2010@hotmail.com>
2025-12-27 23:43:40 -0500

    test style

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Home from './pages/Home';
 const App = () => {
   return (
     <>
-      <h1 className="text-blue-100">test code </h1>
-      {/* <Home /> */}
+      <Home />
     </>
   );
 };
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { getProjects } from '../services/api';
 
-export default function Projects() {
+const Projects = () => {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
@@ -10,42 +10,19 @@ export default function Projects() {
 
   return (
     <section id="projects" className="max-w-6xl mx-auto px-6 py-20">
-      <h2 className="text-3xl font-bold mb-10 text-center">Projects</h2>
-
+      <h2 className="text-3xl font-bold mb-10 text-center"> Projects</h2>
       <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project) => (
           <div
-            key={project.id}
+            key={projects.id}
             className="bg-gray-800 p-6 rounded-xl hover:scale-[1.02] transition"
           >
-            <h3 className="text-xl font-semibold mb-2">{project.title}</h3>
-
+            <h3 className="text-xl font-semibold mb-2"> {project.title} </h3>
             <p className="text-gray-400 mb-4">{project.description}</p>
-
-            <div className="flex gap-4">
-              {project.github_url && (
-                <a
-                  href={project.github_url}
-                  target="_blank"
-                  className="text-indigo-400 hover:underline"
-                >
-                  GitHub
-                </a>
-              )}
-
-              {project.live_url && (
-                <a
-                  href={project.live_url}
-                  target="_blank"
-                  className="text-indigo-400 hover:underline"
-                >
-                  Live
-                </a>
-              )}
-            </div>
           </div>
         ))}
       </div>
     </section>
   );
-}
+};
+export default Projects;
